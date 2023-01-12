f93da885cd34ae7fd7ab2b5431411521721a8b20
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-01-12 14:10:12 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";     // resources > templates에서 hello.html을 찾아서 실행시켜라.
+    }
+}
